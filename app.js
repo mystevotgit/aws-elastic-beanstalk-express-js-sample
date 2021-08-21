aa96678dcc092714ca35682407df48e22ffd20b0
Olatunji Oye <55290410+mystevotgit@users.noreply.github.com>
2021-08-21 21:33:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I added some code to see the effect of the commit on the continuous integration pipline set up on AWS.'));
+app.get('/', (req, res) => res.send('Hello World! I added some text to see the effect of the commit on the continuous integration pipline set up on AWS.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
